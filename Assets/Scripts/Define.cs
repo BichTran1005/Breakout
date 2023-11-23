cbe7043028b09f84a226d1e8f2348954e0f3ba98
BichTran <tranthibich1005@gmail.com>
2023-11-23 14:12:53 +0700

    #DefineScript: Define string

---
+++ Assets/Scripts/Define.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+    public static string BlockGO = "Block";
+    public static string EndGame = "End Game";
+}
